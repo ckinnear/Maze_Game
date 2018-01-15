3164f8f56125e6c8ed10175f8e4455cc69211e3b
ColinKinnear <colin@cortechs.ie>
2018-01-15 14:47:30 +0000

    Added PlayerScore Class with: Method - AddPlayerScore field - scores list

---
+++ Maze_Game/PlayerScore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Game
+{
+    class PlayerScore
+    {
+        //Create a Method to add Scores to a list:
+        public void AddScores(int result)
+        {
+            scores.Add(result);
+        }
+
+        //Create a List to hold scores:
+        public List<int> scores = new List<int>();
+
+    }
+}
--- Maze_Game/Program.cs
+++ Maze_Game/Program.cs
@@ -10,6 +10,14 @@ namespace Maze_Game
     {
         static void Main(string[] args)
         {
+            //Create an instance of the PLayerScore class:
+            PlayerScore score = new PlayerScore();
+
+            //Add values (representing scores) to the list:
+            score.AddScores(8); 
+            score.AddScores(5);
+            score.AddScores(2);
+
         }
     }
 }
